df1e4658e40755c7cb14692f05febb278bc19daa
Xiaoyun <yangx232@gmail.com>
2017-07-27 21:48:51 -0400

    fixed issue in which the carousel would not positioning properly on different devices

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   Text,
+  Dimensions,
   View,
   ListView,
   TouchableHighlight,
@@ -83,6 +84,9 @@ class Calendar extends Component {
 
     const {navigate} = this.props.navigation
 
+    let windowWidth = Dimensions.get('window').width;
+    let calWidth = windowWidth*0.7;
+
     return (
       <View style={{flex: 1}}>
         <View style={{height: 170}}>
@@ -98,8 +102,8 @@ class Calendar extends Component {
             {
               this.state.travelDates.map( (d, i) => {
                 return (
-                  <View key={i} style={[styles.containerCenter, {width: 375}]}>
-                      <View style={{alignItems: 'center', width: 200, backgroundColor: "#e6cf8b", marginTop: -25}}>
+                  <View key={i} style={[styles.containerCenter, {width: windowWidth}]}>
+                      <View style={{alignItems: 'center', width: calWidth, backgroundColor: "#e6cf8b", marginTop: -25}}>
                         <Image source={require('../assets/calendar.png')}
                            style={{width: 50, height: 50, marginTop: 10}} />
                          <Text style={styles.textLarge}>{d.toDateString()}</Text>
